7a0070cd9d48a4946aebdc0516d90f90d4faca1e
Treepyy <105573576+Treepyy@users.noreply.github.com>
2025-06-15 16:33:44 +0800

    refactor processManager

--- processManager.hpp
+++ processManager.hpp
@@ -6,60 +6,13 @@
 class ProcessManager {
 
     public:
-    ProcessManager(int numCores) : cores(numCores) {};
-    std::vector<std::shared_ptr<Process>> process;
-    void makeDummies(int num, int instructions, string text);
-    void addProcess();
-    void UpdateProcessScreen();
+        ProcessManager(int numCores) : cores(numCores) {};
+        std::vector<std::shared_ptr<Process>> process;
+        void makeDummies(int num, int instructions, string text);
+        void addProcess();
+        void UpdateProcessScreen();
+
     private:
-    int cores;
+        int cores;
 };
-
-void ProcessManager::makeDummies(int num, int instructions, string text) {
-    int processNum = num;
-    int numLines = instructions;
-    string output = text;
-    std::string name;
-    int assignedCore;
-    for (int i = 0; i < processNum; i++){
-        if (i < 10){
-            name = "screen_0" + std::to_string(i);
-        } else {
-            name = "screen_" + std::to_string(i);
-        }
-        assignedCore = i % this->cores;
-        process.emplace_back(std::make_shared<Process>(name, i, assignedCore, numLines));
-        for (int j = 0; j < numLines; j++){
-            process[i]->addCommand(output);
-        }
-    }
-}
-
-void ProcessManager::UpdateProcessScreen() {
-    std::cout << "----------------------------------" << std::endl;
-    std::cout << "Running processes: " << std::endl;
-
-    for (const auto& p : process) {
-        if (p->getStatus() == 2) {
-            std::cout << p->getProcessName() << "\t"
-                      << p->getCreationTimestamp() << "\t"
-                      << "Core: " << p->getCoreIndex() << "\t"
-                      << p->getCommandIndex() << "/"
-                      << p->getTotalCommands() << std::endl;
-        }
-    }
-
-    std::cout << "----------------------------------" << std::endl;
-    std::cout << "Finished processes: " << std::endl;
-
-    for (const auto& p : process) {
-        if (p->getStatus() == 3) {
-            std::cout << p->getProcessName() << "\t"
-                      << p->getCreationTimestamp() << "\t"
-                      << "Finished\t"
-                      << p->getTotalCommands() << "/"
-                      << p->getTotalCommands() << std::endl;
-        }
-    }
-}
 
